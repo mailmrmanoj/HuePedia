fa75510bc819a4cd47b7f506b959273567cf9221
Manoj <mailmrmanoj@gmail.com>
2016-04-02 18:14:13 +0530

    fix:added cric hue

--- hue-ui/js/service.js
+++ hue-ui/js/service.js
@@ -19,17 +19,18 @@ app.factory('jsonService', function($http) {
             // Return the promise to the controller
             return promise;
         },
-        postJSON: function() {
+        postJSON: function(id,hue) {
             var req = {
                 method: 'PUT',
-                url: 'http://192.168.1.102/api/ceb443411ac286f16012a5c25fcc5d7/lights/2/state',
+                url: 'http://192.168.1.102/api/ceb443411ac286f16012a5c25fcc5d7/lights/'+id+'/state',
                 headers: {
                     'Content-Type': undefined
                 },
                 data: {
-                    "hue": 70000,
+                    "hue": hue,
                     "on": true,
-                    "bri": 0
+                    "bri": 254,
+                    "sat":254
                 }
             }
             // $http returns a promise, which has a then function, which also returns a promise
@@ -45,6 +46,87 @@ app.factory('jsonService', function($http) {
             });
             // Return the promise to the controller
             return helloPromise;
+        },
+        setSixScore1: function(hue,state,bri) {
+            var req = {
+                method: 'PUT',
+                url: 'http://192.168.1.102/api/ceb443411ac286f16012a5c25fcc5d7/lights/1/state',
+                headers: {
+                    'Content-Type': undefined
+                },
+                data: {
+                    "hue": hue,
+                    "on": state,
+                    "bri": bri
+                }
+            }
+            // $http returns a promise, which has a then function, which also returns a promise
+            var helloPromise = $http(req).then(function(response) {
+                // The then function here is an opportunity to modify the response
+                console.log("big" + response);
+                // The return value gets picked up by the then in the controller.
+                //
+                // je ne sais pas ou il pioche le terme data
+                // peut etre une expression de Angular
+                //
+                return response.data;
+            });
+            // Return the promise to the controller
+            return helloPromise;
+        },
+        setSixScore2: function(hue,state,bri) {
+            var req2 = {
+                method: 'PUT',
+                url: 'http://192.168.1.102/api/ceb443411ac286f16012a5c25fcc5d7/lights/2/state',
+                headers: {
+                    'Content-Type': undefined
+                },
+                data: {
+                    "hue": hue,
+                    "on": state,
+                    "bri": bri
+                }
+            }
+            // $http returns a promise, which has a then function, which also returns a promise
+            var helloPromise2 = $http(req2).then(function(response) {
+                // The then function here is an opportunity to modify the response
+                console.log("big" + response);
+                // The return value gets picked up by the then in the controller.
+                //
+                // je ne sais pas ou il pioche le terme data
+                // peut etre une expression de Angular
+                //
+                return response.data;
+            });
+            // Return the promise to the controller
+            return helloPromise2;
+        },
+        setSixScore3: function(hue,state,bri) {
+            var req2 = {
+                method: 'PUT',
+                url: 'http://192.168.1.102/api/ceb443411ac286f16012a5c25fcc5d7/lights/3/state',
+                headers: {
+                    'Content-Type': undefined
+                },
+                data: {
+                    "hue": hue,
+                    "on": state,
+                    "bri": bri
+                }
+            }
+            // $http returns a promise, which has a then function, which also returns a promise
+            var helloPromise3 = $http(req2).then(function(response) {
+                // The then function here is an opportunity to modify the response
+                console.log("big" + response);
+                // The return value gets picked up by the then in the controller.
+                //
+                // je ne sais pas ou il pioche le terme data
+                // peut etre une expression de Angular
+                //
+                return response.data;
+            });
+            // Return the promise to the controller
+            return helloPromise3;
         }
     }
 });
