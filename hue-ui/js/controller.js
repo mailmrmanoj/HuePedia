6d27e12b0dd3e82683255d4a8844fd3ba09e8d92
Manoj <mailmrmanoj@gmail.com>
2016-04-02 12:47:56 +0530

    fix:added api changes

---
+++ hue-ui/js/controller.js
@@ -0,0 +1,89 @@
+//(function(angular) {
+//    'use strict';
+//
+//    function MyController(AnnyangService,jsonService, $scope) {
+//        var vm = this;
+//        jsonService.getjson().then(function(d) {
+//            console.log("bigctrl" + d);
+//            vm.avengers = d[0].acteurs;
+//            $scope.loading =false;
+//        });
+//        vm.init = function() {
+//            vm.clearResults();
+//
+//            AnnyangService.addCommand('*allSpeech', function(allSpeech) {
+//                console.debug(allSpeech);
+//                vm.addResult(allSpeech);
+//            });
+//
+//            AnnyangService.start();
+//        };
+//
+//        vm.addResult = function(result) {
+//            vm.results.push({
+//                content: result,
+//                date: new Date()
+//            });
+//        };
+//
+//        vm.clearResults = function() {
+//            vm.results = [];
+//        };
+//
+//        vm.init();
+//    }
+//
+//    angular.module('myApp')
+//        .controller('MyController', MyController);
+//
+//}(window.angular));
+
+app.controller('MainCtrl', function ($scope, AnnyangService, jsonService) {
+    $scope.name = 'World';
+    $scope.loading = true;
+    jsonService.getjson().then(function (d) {
+        console.log("bigctrl" + d);
+        $scope.avengers = d[0].acteurs;
+        $scope.loading = false;
+    });
+
+    $scope.init = function () {
+        $scope.clearResults();
+
+        AnnyangService.addCommand('*allSpeech', function (allSpeech) {
+            console.debug(allSpeech);
+            $scope.addResult(allSpeech);
+        });
+
+        AnnyangService.start();
+    };
+    function igniteHello(){
+        
+    }
+
+    function checkKeywords(result) {
+        if (result == "hello") {
+            igniteHello();
+        }
+    }
+
+    $scope.addResult = function (result) {
+        $scope.results.push({
+            content: result,
+            date: new Date()
+        });
+        checkKeywords(result);
+
+    };
+
+    $scope.clearResults = function () {
+        $scope.results = [];
+    };
+
+    $scope.init();
+
+
+});
+
+
+
---
+++ hue-ui/js/service.js
@@ -0,0 +1,24 @@
+/**
+ * Created by manoj on 4/2/2016.
+ */
+app.factory('jsonService', function($http) {
+    return {
+
+        getjson: function() {
+            //alert("ok");
+            // $http returns a promise, which has a then function, which also returns a promise
+            var promise = $http.get('https://api.mongolab.com/api/1/databases/lagrossetete/collections/avengers?apiKey=j0PIJH2HbfakfRo1ELKkX0ShST6_F78A').then(function(response) {
+                // The then function here is an opportunity to modify the response
+                console.log("big" + response);
+                // The return value gets picked up by the then in the controller.
+                //
+                // je ne sais pas ou il pioche le terme data
+                // peut etre une expression de Angular
+                //
+                return response.data;
+            });
+            // Return the promise to the controller
+            return promise;
+        }
+    }
+});
